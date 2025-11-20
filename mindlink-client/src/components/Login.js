1eda531a72eb84290c15de1901fd458b77778b94
Adam556678 <mahmudramadan75@yahoo.com>
2025-11-20 17:21:52 +0200

    changing login page ui

--- mindlink-client/src/components/Login.js
+++ mindlink-client/src/components/Login.js
@@ -4,7 +4,7 @@ import { Form, Button, Card } from "react-bootstrap";
 export default function Login() {
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
-      <Card className="w-50 w-sm-75 w-md-50 p-4 shadow-lg rounded-4">
+      <Card className="col-sm-8 col-lg-4 col-md-6 p-4 shadow-lg rounded-4">
         <Card.Body>
           <Card.Title className="fw-bold mb-4 text-center fs-2">
             Login
@@ -36,7 +36,7 @@ export default function Login() {
             <Button 
               variant="primary" 
               type="submit"
-              className="w-50 py-2 rounded-4 fw-semibold"
+              className="w-25 py-2 rounded-4 fw-semibold d-block mx-auto"
             >
               Login
             </Button>
