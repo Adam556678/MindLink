0bb4a59753684451698d66c5c056518d7957a675
Adam556678 <mahmudramadan75@yahoo.com>
2025-11-21 12:07:57 +0200

    Register user controller

---
+++ MindLinkAPI/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using mindlinkapi.Entities;
+using MindLinkAPI.Models;
+using MindLinkAPI.Services;
+
+namespace MindLinkAPI.Controllers
+{   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IAuthService authService) : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserRegisterDto request)
+        {
+            var user = await authService.RegisterAsync(request); 
+            if (user == null)
+            {
+                return BadRequest("Email is already used");
+            }
+
+            return Ok(user);
+        }
+    }
+}
--- MindLinkAPI/obj/Debug/net9.0/MindLinkAPI.AssemblyInfo.cs
+++ MindLinkAPI/obj/Debug/net9.0/MindLinkAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MindLinkAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dd542d5c19d22388e74a946d4f1513d45c14a85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed2f02df9a8dabcbd3f5cba39f1ab8f43cc9fd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MindLinkAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MindLinkAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
