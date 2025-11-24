39c50330505ed0039812200022ced23bd8332a0d
Adam556678 <mahmudramadan75@yahoo.com>
2025-11-24 10:14:18 +0200

    Implement the Navigation Bar

--- mindlink-client/src/App.js
+++ mindlink-client/src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from './components/Login';
 import Register from './components/Register';
+import NavBar from './components/NavBar';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/register' element={<Register />} />
       </Routes>
     </BrowserRouter>
+
   );
 }
 
---
+++ mindlink-client/src/components/NavBar.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Navbar, Container, Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <Navbar bg="primary" className="px-3 py-2" style={{height:"65px"}}>
+      {/* <Container fluid className="d-flex justify-content-between align-items-center"> */}
+      <Container fluid className="justify-content-start position-relative">
+
+        {/* LEFT: Brand + Input + Button */}
+        <div className="d-flex align-items-center gap-2">
+          <h2 className="m-0 me-4">
+            <Link to="/" className="link-light text-decoration-none">
+              <i className="bi bi-pencil me-2 fs-3"></i>
+              MindLink
+            </Link>
+          </h2>
+
+          <Form.Control
+            placeholder="Enter Code..."
+            className="d-none d-md-inline"
+            style={{
+                borderRadius: "20px 0 0 20px", // left rounded only
+                height: "38px",
+                width: "150px",
+                border: "none",
+                paddingLeft: "15px",
+            }}
+            />
+
+            <Button
+            variant="light"
+            className="fw-semibold d-none d-md-inline"
+            style={{
+                borderRadius: "0 20px 20px 0", // right rounded only
+                border: "none",
+                height: "38px",
+            }}
+            >
+            GO
+            </Button>      
+        </div>
+
+        {/* CENTER: User Info */}
+        {/* <div className="d-flex align-items-center text-white fw-semibold fs-5 gap-2"> */}
+        <div className="d-flex align-items-center text-white fw-semibold fs-5 gap-2 
+        position-absolute start-50 translate-middle-x">
+          <i className="bi bi-person-fill fs-4"></i>
+          Mahmoud
+        </div>
+
+        {/* RIGHT: Menu Button */}
+        <div className="d-flex align-items-center position-absolute end-0">
+          <Button>
+            <i className="bi bi-list fs-3"></i>
+          </Button>
+        </div>
+
+      </Container>
+    </Navbar>
+  );
+}
