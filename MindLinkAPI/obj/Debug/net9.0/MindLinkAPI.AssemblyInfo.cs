00a46a67a362ad0c5bb62e7a834a2dad019a5f68
Adam556678 <mahmudramadan75@yahoo.com>
2025-11-28 03:55:38 +0200

    Your quizzes home page section

--- MindLinkAPI/obj/Debug/net9.0/MindLinkAPI.AssemblyInfo.cs
+++ MindLinkAPI/obj/Debug/net9.0/MindLinkAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MindLinkAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f87d13abd5be0c8d1f2dea01c8f4dd4e696b711")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f62445f5462c39ab0686a0259119f8dc403f9d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MindLinkAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MindLinkAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- mindlink-client/src/pages/Home.js
+++ mindlink-client/src/pages/Home.js
@@ -1,45 +1,73 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import './Home.css';
 
 export default function Home() {
   return (
-    <div className='px-3 py-4'>
-        <h2 className='mb-4'>
+    <div style={{marginTop:65}} className='px-3 py-4'>
+        <div className='mb-5'>
+            <h2 className='mb-4'>
             Browse Categories
-        </h2>
+            </h2>
 
-        <Card style={{
-        width:'200px', 
-        height:'180px', 
-        backgroundColor:'#ffc0cb', 
-        border:'none', 
-        overflow:'hidden', 
-        display:'flex', 
-        }} 
-        className='shadow position-relative'
-        >
-            <div style={{
-                height: '75%',        
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}>
-                <i className="bi bi-flask fs-1"></i>
-            </div>           
+            <Card style={{
+                width:'200px', 
+                height:'180px', 
+                backgroundColor:'#ffc0cb', 
+                border:'none', 
+                overflow:'hidden', 
+                display:'flex', 
+            }} 
+            className='shadow position-relative'
+            >
+                <div style={{
+                    height: '75%',        
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                    <i className="bi bi-flask fs-1"></i>
+                </div>           
 
-            <div style={{
-            width:'100%', 
-            height:'25%', 
-            backgroundColor:'#f3f6f8ff', 
-            position:'absolute', 
-            bottom:0, 
-            display:'flex', 
-            justifyContent:'center', 
-            alignItems:'center', 
-            fontSize:20}}>
-                Science
+                <div style={{
+                    width:'100%', 
+                    height:'25%', 
+                    backgroundColor:'#f3f6f8ff', 
+                    position:'absolute', 
+                    bottom:0, 
+                    display:'flex', 
+                    justifyContent:'center', 
+                    alignItems:'center', 
+                    fontSize:20}}>
+                    Science
+                </div>
+            </Card>
+        </div>
+
+        <div>
+            <h2 className='mb-4'>
+                Your Created Quizzes
+            </h2>
+            
+            <div className='col-sm-6 col-lg-3'>
+                <Card className='yourQuizzes rounded-0'>
+                    <Card.Body style={{position:'relative'}}>
+                        <div className='fw-semibold mb-2'>Javascript Fundamentals</div>
+                        <div className='mb-3'>Questions: 15</div>
+                        <div  className='quizCode'>
+                            <i class="bi bi-clipboard"></i>
+                            <span className='fw-semibold'>XYW3Z#</span>
+                        </div>
+                        <ul className='myQuizzesLst'>
+                            <li><a href='#'>Edit</a></li>
+                            <li><a href='#'>View Results</a></li>
+                        </ul>
+                    </Card.Body>
+                </Card>
             </div>
-        </Card>
+
+            
+        </div>
 
     </div>
   )
