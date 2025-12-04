da79c94fa57f381d242da9afff3ef333ef09004e
Adam556678 <mahmudramadan75@yahoo.com>
2025-12-04 20:15:45 +0200

    Implement the App Drawer

--- mindlink-client/src/components/NavBar.js
+++ mindlink-client/src/components/NavBar.js
@@ -1,14 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Navbar, Container, Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import Drawer from '@mui/material/Drawer';
+import "./NavBar.css";
+import { useNavigate } from "react-router-dom";
 
 export default function NavBar() {
 
   const {user} = useContext(AuthContext); 
+  
+  const [open, setOpen] = useState(false);
+
+  const navigate = useNavigate();
+  
+  const toggleDrawer = (openDrawer) => {
+    setOpen(openDrawer);
+  }
+
+  const handleNavigation = (route) => {
+    setOpen(false);
+    navigate(route);
+  } 
 
   return (
-    <Navbar bg="primary" className="px-3 py-2" style={{height:"65px", position:'fixed', top: 0,
+    <>
+      <Navbar bg="primary" className="px-3 py-2" style={{height:"65px", position:'fixed', top: 0,
     left: 0,
     right: 0,
     zIndex: 1000}}>
@@ -59,12 +76,66 @@ export default function NavBar() {
 
         {/* RIGHT: Menu Button */}
         <div className="d-flex align-items-center position-absolute end-0">
-          <Button>
+          <Button
+            onClick={() => toggleDrawer(true)}
+          >
             <i className="bi bi-list fs-3"></i>
           </Button>
         </div>
 
       </Container>
     </Navbar>
+
+    {/* Global Drawer */}
+      <Drawer 
+        open={open} 
+        onClose={() => toggleDrawer(false)}
+        anchor="right"
+        sx={{
+        '& .MuiDrawer-paper': {
+        width: '20%',
+        height: '100%',
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'space-between'
+        // backgroundColor:"#ddd"
+        }
+    }}
+    >
+        <div>
+          <div className="createQuiz d-flex gap-2 align-items-center justify-content-center" 
+            onClick={()=>handleNavigation("/add-quiz")}
+          >
+            <i class="bi bi-pencil-fill"></i>
+            <span >Create a Quiz</span>
+          </div>
+
+
+          <div className="drawer-item">
+            <i class="bi bi-person-fill"></i>
+            <span className="drawer-item-text">Profile</span>
+          </div>
+          <div className="drawer-item" onClick={()=>handleNavigation("/your-quizzes")}>
+            <i class="bi bi-lightbulb-fill"></i>
+            <span className="drawer-item-text">My Quizzes</span>
+          </div>
+          <div className="drawer-item">
+            <i class="bi bi-clock-history"></i>
+            <span className="drawer-item-text">Quizzes I took</span>
+          </div>
+        </div>
+
+        <div>
+          <div className="drawer-item mb-3">
+            <i class="bi bi-box-arrow-right"></i>
+            <span className="drawer-item-text">Log Out</span>
+          </div>
+
+        </div>
+
+    </Drawer>
+
+    </>
+    
   );
 }
