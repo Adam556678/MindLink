03c6f4021c041f5c2bca8aa5af3cf8cca988e906
Adam556678 <mahmudramadan75@yahoo.com>
2025-12-15 15:43:50 +0200

    Add screenshots

--- MindLinkAPI/obj/Debug/net9.0/MindLinkAPI.AssemblyInfo.cs
+++ MindLinkAPI/obj/Debug/net9.0/MindLinkAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MindLinkAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42d11bdb3909de9a8c0b7accacab406748eaee6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54154e9ff50188d550c0b792c1834cb2e7e0acd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MindLinkAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MindLinkAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
